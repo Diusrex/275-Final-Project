6ee5775625d3a31cb6e1cedf978f01502f3cce08
Diusrex <killakan002@hotmail.com>
2014-04-04 11:13:26 -0600

    Changing of names to lowercase in TicTacToe pt 2.

---
+++ TicTacToe/positionInfo.py
@@ -0,0 +1,19 @@
+class PositionInfo:
+    """
+    This class's sole purpose is to store position information for the game easily, rather than have it be scattered among 4 or 5 different lists
+    
+    """
+    
+    def __init__(self, startX, startY):
+        self.startPosX = [startX, -1, -1, -1]
+        self.startPosY = [startY, -1, -1, -1]
+        
+        # The third value in these is just for ease, will be removed
+        self.lineSideStartX = [-1, -1, -1]
+        self.lineSideStartY = [-1, -1, -1]
+        
+        self.lineStartX = -1
+        self.lineStartY = -1
+        
+        self.lineLengthX = -1
+        self.lineLengthY = -1
